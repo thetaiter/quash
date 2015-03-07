32579e24d5ee291c2cc058f404c06af5b36d50f0
Ian Tait <thetaiter@gmail.com>
2015-03-06 18:59:54 -0600

    Added code for tokenzing input and setup set command

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <readline/readline.h>
 
 char *trimWhiteSpaces(char *str) {
@@ -30,12 +31,50 @@ char *trimWhiteSpaces(char *str) {
     return str;
 }
 
+char **tokenize(char* input) {
+	char **ret = NULL;
+	char *temp = strtok(input, " ");
+	int i, numSpaces = 0;
+
+	while(temp) {
+		ret = realloc(ret, sizeof(char*) * ++numSpaces);
+
+		if (ret == NULL) {
+			printf("\nMemory allocation failed for command '%s'\n\n", input);
+			return ret;
+		}
+
+		// If token is a string, put entire string in token and remove quotes
+		if (temp[0] == '"') {
+			temp++;
+			sprintf(temp, "%s%s%s", temp, " ", strtok(NULL, "\""));
+		}
+
+		ret[numSpaces-1] = temp;
+
+		temp = strtok(NULL, " =");
+	}
+
+	ret = realloc(ret, sizeof(char*) * (numSpaces+1));
+	ret[numSpaces] = 0;
+
+	return ret;
+}
+
+void handleSigInt(int sig) {
+	printf("\n\nSIGINT signal %i received. Quitting quash.\n\n", sig);
+	exit(0);
+}
+
 int main(int argc, char **argv, char **envp) {
     // Initialize Variables
-    char *input,  prompt[128];
+    char *input, prompt[128];
     char *user = getenv("USER");
     char *home = getenv("HOME");
     char *path = getenv("PATH");
+
+    // Check if SIGINT signal is received and set handler
+    signal(SIGINT, handleSigInt);
 
     // Ininite Loop
     while(1) {
@@ -54,17 +93,40 @@ int main(int argc, char **argv, char **envp) {
                 // Add the current input to the history
                 add_history(input);
 
-                // Get the first token in the string, delimiting at spaces and = signs
-                char *token = strtok(input, " =");
-            
+                // Tokenize input and place tokens into an array
+                char **tokens = tokenize(input);
+
                 // If the first token is equal to "exit" or "quit"
-                if (strcmp("exit", token) == 0 || strcmp("quit", token) == 0) {
-                    // Break out of the infinite loop and terminal the program
+                if (strcmp("exit", tokens[0]) == 0 || strcmp("quit", tokens[0]) == 0) {
+                    // Break out of the infinite loop and terminate the program
                     break;
                 }
 
+                // If the first token is 'set'
+                if (strcmp("set", tokens[0]) == 0) {
+                	// Get the variable to be set and the value to set it to
+                	char *newVar = tokens[1];
+                	char *value = tokens[2];
 
+            		setenv(newVar, value, 0);
+
+            		if (strcmp(value, getenv(newVar)) == 0) {
+            			printf("\nNew environment variable '%s' was set to the value '%s' successfully.\n\n", newVar, getenv(newVar));
+            		} else {
+            			printf("\nValue for '%s' was already set to '%s'.\nWould you like to reset this environment variable to '%s'?", newVar, getenv(newVar), value);
+            			char *temp = readline(" (y/n): ");
+
+            			if (strcasecmp(temp, "y") == 0 || strcasecmp(temp, "yes") == 0) {
+            				setenv(newVar, value, 1);
+            				printf("Environment variable '%s' was set to the value '%s' successfully.\n\n", newVar, getenv(newVar));
+            			} else {
+            				printf("%s", "\n");
+         					}
+            		}
+                }
             }
         }
     }
+
+    printf("%s", "\nThank you for using this quash program. Have a wonderful day!\n\n");
 }
